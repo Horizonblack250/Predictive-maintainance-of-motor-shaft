92985ff95a076c78cadf73a2af5e6182a92954a0
Horizonblack250 <155500180+Horizonblack250@users.noreply.github.com>
2024-10-19 13:28:23 +0530

    Updated Final.py

--- Final.py
+++ Final.py
@@ -3,32 +3,22 @@ from hmmlearn import hmm
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 
-# Load your dataset into a pandas DataFrame
-# Replace 'your_dataset.csv' with the actual file path of your dataset
 data = pd.read_csv('0D.csv')
 
-# Select the columns you want to use for modeling
-# Here, we're using Vibration_1, Vibration_2, Vibration_3, Measured_RPM, and Vin
 selected_columns = ['Vibration_1', 'Vibration_2', 'Vibration_3', 'Measured_RPM', 'V_in']
 observations = data[selected_columns].values
 
-# Normalize the data
 scaler = StandardScaler()
 observations = scaler.fit_transform(observations)
 
-# Create an HMM model with an increased number of hidden states
-num_states = 4  # Adjust the number of hidden states
+num_states = 4  
 model = hmm.GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=100, tol=1e-3)
 
-# Train the HMM on your dataset
 model.fit(observations)
 
-# Perform inference using the Viterbi algorithm to find the most likely state sequence
 state_sequence = model.predict(observations)
 
-# You can also compute the likelihood of the observations given the model
 log_likelihood = model.score(observations)
 
-# Print the results
 print("Most likely state sequence:", state_sequence)
 print("Log likelihood of observations:", log_likelihood)
